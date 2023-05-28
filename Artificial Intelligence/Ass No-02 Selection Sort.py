8ae3e2d81d6ca9f501ffccbb411740309542d849
Kunal Nandre <111383966+kunalnandre@users.noreply.github.com>
2023-05-28 23:04:00 +0530

    Update Ass No-02 Selection Sort.py

--- Artificial Intelligence/Ass No-02 Selection Sort.py
+++ Artificial Intelligence/Ass No-02 Selection Sort.py
@@ -16,3 +16,25 @@ for i in range(n):
     element=int(input("Enter List Element: "))
     arr.append(element)
 print("\nSorted List is:", selection_sort_greedy(arr))
+
+
+# Output:
+
+# Length of List: 6
+# Enter List Element: 4
+# Enter List Element: 5
+# Enter List Element: 89
+# Enter List Element: 56
+# Enter List Element: 2
+# Enter List Element: 55
+
+# List before Sorting:  [4, 5, 89, 56, 2, 55] 
+
+# List After Pass  1 :  [2, 5, 89, 56, 4, 55]
+# List After Pass  2 :  [2, 4, 89, 56, 5, 55]
+# List After Pass  3 :  [2, 4, 5, 56, 89, 55]
+# List After Pass  4 :  [2, 4, 5, 55, 89, 56]
+# List After Pass  5 :  [2, 4, 5, 55, 56, 89]
+# List After Pass  6 :  [2, 4, 5, 55, 56, 89]
+
+# Sorted List is: [2, 4, 5, 55, 56, 89]
